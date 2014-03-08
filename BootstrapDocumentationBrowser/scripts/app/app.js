1773b686406ddb5c16db732427568679985a2bd0
sevenzerothree <ctobin@thesevenzerothree.com>
2014-03-08 15:09:32 -0500

    General Idea

---
+++ BootstrapDocumentationBrowser/scripts/app/app.js
@@ -0,0 +1,11 @@
+﻿var app = function () {
+    var _init = function () {
+        events.init();
+    };
+
+    return {
+        init: _init
+    }
+}();
+
+app.init();
---
+++ BootstrapDocumentationBrowser/scripts/app/events.js
@@ -0,0 +1,33 @@
+﻿var events = function () {
+    var _init = function () {
+        _keyboardEvents();
+    }
+    _keyboardEvents = function () {
+        var textToSearchFor;
+
+        var _search = function () {
+            var $this = $(this),
+                thisText = $this.text().toLowerCase();
+            if (thisText.search(textToSearchFor) > -1) {
+                // The search input matches
+                $this.show();
+            }
+            else {
+                $this.hide();
+            }
+        };
+
+        $('#search').keyup(function () {
+            var $searchInput = $(this);
+            textToSearchFor = $searchInput.val().toLowerCase();
+
+            $('#accordion .panel-title > a').each(_search);
+
+            $('#documentation-list > li > a').each(_search);
+        });
+    };
+
+    return {
+        init: _init
+    };
+}();
